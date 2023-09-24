3234ec3cf88a8f2994dbab5dbd0989ed6cd49e90
liMilCo <91998494+liMilCo@users.noreply.github.com>
2023-09-24 19:54:49 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 echo "#!/bin/bash" > /home/check_network
-echo "x=\"ping -c1 google.com 2>&1 | grep unknown\"" >> /home/check_network
+echo "x=\`ping -c1 google.com 2>&1 | grep unknown\`" >> /home/check_network
 echo "LOGFILE=\"/home/hiddify-panel/check_network.log\"" >> /home/check_network
 echo "TIMESTAMP=\`date "+%Y-%m-%d %H:%M:%S"\`" >> /home/check_network
 echo "if [ ! \"\$x\" = \"\" ]; then" >> /home/check_network

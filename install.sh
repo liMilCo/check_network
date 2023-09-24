fb87a1ddef4011636c0b25006ebc84c22f302cdc
liMilCo <91998494+liMilCo@users.noreply.github.com>
2023-09-24 19:46:36 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
-echo text >> /home/f2.txt
+echo "#!/bin/bash" >> /home/check_network
+echo "x=`ping -c1 google.com 2>&1 | grep unknown`" >> /home/check_network
+echo "LOGFILE=\"/home/hiddify-panel/check_network.log\"" >> /home/check_network
+echo "TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`" >> /home/check_network
+echo "if [ ! \"$x\" = \"\" ]; then" >> /home/check_network
+echo "echo \"Net is down!! Attempting to restart at $TIMESTAMP.\"" >> /home/check_network
+echo "systemctl restart systemd-networkd" >> /home/check_network
+echo "fi" >> /home/check_network

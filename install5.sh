cabf59c8ea54e7151c1baed7f6f2528824b49c52
liMilCo <91998494+liMilCo@users.noreply.github.com>
2023-09-24 20:42:47 +0330

    Update and rename install4.sh to install5.sh

--- install5.sh
+++ install5.sh
@@ -1,7 +1,7 @@
 echo "#!/bin/bash" > /home/check_network
 echo "x=\`ping -c1 google.com 2>&1 | grep unknown\`" >> /home/check_network
 echo "LOGFILE=\"/home/check_network.log\"" >> /home/check_network
-echo "TIMESTAMP=\`date \"+%Y-%m-%d %H:%M:%S"\`\"" >> /home/check_network
+echo "TIMESTAMP=\`date \"+%Y-%m-%d %H:%M:%S\"\`" >> /home/check_network
 echo "if [ ! \"\$x\" = \"\" ]; then" >> /home/check_network
 echo " echo \"Net is down!! Attempting to restart at \$TIMESTAMP.\"" >> /home/check_network
 echo " systemctl restart systemd-networkd" >> /home/check_network
